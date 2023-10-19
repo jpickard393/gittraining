a2ed28f3f5177fdc409c8e4e3116f7109eaeaed7
julianpickard <jpickard@and.digital>
2023-10-19 15:58:45 +0100

    Hello

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,10 @@ function App() {
       </div>
       <h2 className="read-the-docs">This is feature/JP1</h2>
       <h2>This change has been added</h2>
+      <h2>H1s now H2s</h2>
+      <h2>This is feature/jp3</h2>
+      <h2>This was addede after git reset --hard 3f699c4</h2>
+      <h2>This is a new addition to feature/jp3</h2>
       <h1>Hello</h1>
     </>
   );
