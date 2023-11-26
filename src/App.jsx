d0e6a363dc0716d0fe17cbcb68c85b65cf9e244f
julianpickard <jpickard@and.digital>
2023-11-26 14:45:49 +0000

    Cleaned Up

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <h2 className="read-the-docs">This is feature/JP1</h2>
       <h1>This is feature/JP2</h1>
     </>
   );
